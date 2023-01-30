0496e6afca6866780a2b6370fa22b0cd9762bd22
shakil89427 <shakilahmed89427@gmail.com>
2023-01-30 23:27:55 +0600

    user redesigned

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,5 +1,13 @@
 import { useParams } from "react-router-dom";
 import useGetUser from "../hooks/queries/useGetUser";
+import {
+  AiFillFacebook,
+  AiFillTwitterSquare,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaWhatsappSquare } from "react-icons/fa";
 import mainlogo from "../assets/images/mainlogo.png";
 
 const User = () => {
@@ -15,53 +23,60 @@ const User = () => {
 
   return (
     <div className="flex items-center justify-center w-full h-full bg-black">
-      <div className="w-full h-full md:w-[450px] md:h-fit md:max-h-[90%] md:rounded-xl overflow-hidden flex flex-col items-start justify-start pb-2 bg-white">
-        <img
-          src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"
-          alt=""
-          className="w-full aspect-[2/1] shrink-0"
-        />
-        <div className="relative w-36 h-36 mx-auto -mt-20 bg-white p-1.5 rounded-full px-2 shrink-0">
-          <img
-            src={data?.profileImageUrl}
-            alt=""
-            className="w-full h-full rounded-full object-cover object-center"
-          />
-          <div className="absolute w-16 h-16 bg-white p-1 rounded-full right-0 bottom-0 translate-x-2">
+      <div className="w-full h-full md:max-w-[450px] md:h-[90%] md:max-h-[850px] overflow-hidden bg-white">
+        <div className="h-16 bg-blue-200/60 relative flex items-center pl-5">
+          <img src={mainlogo} alt="" className="w-28" />
+          <div className="absolute bg-blue-400 w-72 h-72 rounded-full right-0 top-0 translate-x-1/2 -translate-y-2/3" />
+        </div>
+        <div className="px-5">
+          <div className="flex items-center justify-between mt-5 text-center">
             <img
-              src={mainlogo}
+              src={data?.profileImageUrl}
               alt=""
-              className="w-full h-full rounded-full object-cover object-center"
+              className="w-20 h-20 rounded-full object-cover object-center"
             />
+            <div>
+              <p className="font-bold">2</p>
+              <p className="text-sm text-gray-600">Followers</p>
+            </div>
+            <div>
+              <p className="font-bold">1</p>
+              <p className="text-sm text-gray-600">Following</p>
+            </div>
           </div>
-        </div>
-        <div className="text-center my-4 font-medium px-2 shrink-0 w-full">
-          <p className="text-lg md:text-xl lg:text-2xl font-semibold">{data?.name}</p>
-          <p className="text-sm lg:text-base text-gray-500">Los Angeles,CA</p>
-          <p className="text-sm lg:text-base text-gray-500 mt-2">{data?.bio}</p>
-          <button className="w-full h-12 max-w-[300px] block mx-auto bg-black mt-5 text-white rounded-full text-base font-semibold duration-300 hover:scale-105">
-            Connect
+          <div className="mt-2">
+            <p className="font-semibold text-blue-500">{data?.name}</p>
+            <p className="text-xs mt-1">{data?.bio}</p>
+          </div>
+          <button className="bg-blue-500 h-12 w-full mt-5 rounded-lg text-white font-medium">
+            Contact
           </button>
-        </div>
-        <div className="px-2 overflow-y-auto grow scroller">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repellat facilis tempora
-          consequatur iste minima officiis quisquam in temporibus quis similique architecto, odit
-          nisi quam officia asperiores. Eum eaque accusantium, iusto autem dignissimos eos accusamus
-          architecto, tempore, impedit ab blanditiis magnam! Earum accusantium ipsa dolorem
-          reiciendis, fugiat quam odio alias facilis maxime nostrum recusandae sapiente vel repellat
-          nesciunt voluptas, dignissimos voluptatum voluptatibus iusto illo eveniet praesentium
-          illum, eum consectetur dolore? Ad adipisci, alias mollitia incidunt officiis in culpa
-          iste, nisi tempora dolorum ullam error vitae inventore fugit minus ex architecto dicta et
-          pariatur. Asperiores est magni eius illum atque vero! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Corrupti repellat facilis tempora consequatur iste minima
-          officiis quisquam in temporibus quis similique architecto, odit nisi quam officia
-          asperiores. Eum eaque accusantium, iusto autem dignissimos eos accusamus architecto,
-          tempore, impedit ab blanditiis magnam! Earum accusantium ipsa dolorem reiciendis, fugiat
-          quam odio alias facilis maxime nostrum recusandae sapiente vel repellat nesciunt voluptas,
-          dignissimos voluptatum voluptatibus iusto illo eveniet praesentium illum, eum consectetur
-          dolore? Ad adipisci, alias mollitia incidunt officiis in culpa iste, nisi tempora dolorum
-          ullam error vitae inventore fugit minus ex architecto dicta et pariatur. Asperiores est
-          magni eius illum atque vero!
+          <div className="grid grid-cols-2 gap-3 mt-5">
+            <button className="bg-gray-200 rounded-lg h-12 flex items-center justify-start px-3 font-medium">
+              <AiFillFacebook className="text-4xl mr-3" />
+              <p>Facebook</p>
+            </button>
+            <button className="bg-gray-200 rounded-lg h-12 flex items-center justify-start px-3 font-medium">
+              <AiFillTwitterSquare className="text-4xl mr-3" />
+              <p>Twitter</p>
+            </button>
+            <button className="bg-gray-200 rounded-lg h-12 flex items-center justify-start px-3 font-medium">
+              <AiFillInstagram className="text-4xl mr-3" />
+              <p>Instagram</p>
+            </button>
+            <button className="bg-gray-200 rounded-lg h-12 flex items-center justify-start px-3 font-medium">
+              <AiFillLinkedin className="text-4xl mr-3" />
+              <p>Linkedin</p>
+            </button>
+            <button className="bg-gray-200 rounded-lg h-12 flex items-center justify-start px-3 font-medium">
+              <AiFillGithub className="text-4xl mr-3" />
+              <p>Github</p>
+            </button>
+            <button className="bg-gray-200 rounded-lg h-12 flex items-center justify-start px-3 font-medium">
+              <FaWhatsappSquare className="text-4xl mr-3" />
+              <p>Whatsapp</p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
