4cd58988af38494a6a0f51f03ffc76cbb22ce38e
shakil89427 <shakilahmed89427@gmail.com>
2023-02-06 20:23:33 +0600

    checked

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (_, res) => res.send("Server running"));
-app.use("/api/user", require("./src/routes/user.route"));
-app.use("/api/product", require("./src/routes/product.route"));
-app.use("/api/banner", require("./src/routes/banner.route"));
+app.get("/", (_, res) => res.send("Server running"));
+app.use("/user", require("./src/routes/user.route"));
+app.use("/product", require("./src/routes/product.route"));
+app.use("/banner", require("./src/routes/banner.route"));
 app.use(require("./src/helpers/routeCatcher"));
 app.use(require("./src/helpers/errorHandler"));
 
