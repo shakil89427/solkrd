08ce622bd6d66a318c1eae236440ef4ff97f52bd
shakil89427 <shakilahmed89427@gmail.com>
2023-02-09 20:54:56 +0600

    products sorted in desc

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -2,7 +2,11 @@ const firestore = require("../firebase/admin").firestore();
 
 module.exports.getProducts = async (req, res, next) => {
   try {
-    const response = await firestore.collection("products").where("active", "==", true).get();
+    const response = await firestore
+      .collection("products")
+      .where("active", "==", true)
+      .orderBy("inPrice", "desc")
+      .get();
     const products = response.docs.map((item) => item.data());
     res.send(products);
   } catch (error) {
