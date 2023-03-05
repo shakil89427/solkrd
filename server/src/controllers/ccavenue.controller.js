8fbc9b5fc07934f9b697abff71673014f62589d5
shakil89427 <shakilahmed89427@gmail.com>
2023-03-05 16:52:59 +0600

    ccavenue updated

--- server/src/controllers/ccavenue.controller.js
+++ server/src/controllers/ccavenue.controller.js
@@ -2,8 +2,9 @@ const admin = require("../firebase/admin");
 
 module.exports.success = async (req, res, next) => {
   try {
-    await admin.firestore().collection("ccsuccess").add(req.body);
-    res.redirect("https://solkrd.com/payment/success");
+    const { encResp, orderNo } = req.body;
+    await admin.firestore().collection("ccsuccess").add({ encResp, orderNo });
+    res.redirect(`https://solkrd.com/payment/success?encResp=${encResp}&orderNo=${orderNo}`);
   } catch (error) {
     res.end();
   }
@@ -11,8 +12,9 @@ module.exports.success = async (req, res, next) => {
 
 module.exports.cancel = async (req, res, next) => {
   try {
-    await admin.firestore().collection("cccancel").add(req.body);
-    res.redirect("https://solkrd.com/payment/failed");
+    const { encResp, orderNo } = req.body;
+    await admin.firestore().collection("cccancel").add({ encResp, orderNo });
+    res.redirect(`https://solkrd.com/payment/failed?encResp=${encResp}&orderNo=${orderNo}`);
   } catch (error) {
     res.end();
   }
